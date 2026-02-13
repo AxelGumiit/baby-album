ede03171ac67c0682c0fa16bded334d5455c7c17
Axel Gumiit <149522466+AxelGumiit@users.noreply.github.com>
2026-02-13 15:24:36 +0000

    Update admin.py

--- backend/album/admin.py
+++ backend/album/admin.py
@@ -3,6 +3,4 @@ from .models import Photo
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('name', 'uploaded_at')
-    search_fields = ('name',) 
     list_filter = ('uploaded_at',)
